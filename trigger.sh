39e9da68effe4e81105f72dc4972a52c79098c3f
Kittipun Khantitrirat <boot191@gmail.com>
2016-12-28 00:00:07 +0700

    Added more triggers

--- trigger.sh
+++ trigger.sh
@@ -7,3 +7,7 @@ TRAVIS_BODY='{
 }}'
 
 curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "$TRAVIS_BODY" https://api.travis-ci.org/repo/3d-pro%2Fnginx/requests
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "$TRAVIS_BODY" https://api.travis-ci.org/repo/3d-pro%2Fapache2/requests
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "$TRAVIS_BODY" https://api.travis-ci.org/repo/3d-pro%2Fnode/requests
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "$TRAVIS_BODY" https://api.travis-ci.org/repo/3d-pro%2Fwebpy/requests
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d "$TRAVIS_BODY" https://api.travis-ci.org/repo/3d-pro%2Fdockvpn/requests
